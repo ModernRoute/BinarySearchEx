b12c31a107c9e6050e8b131d637d53849972b739
Anton Podavalov <a.podavalov@gmail.com>
2026-01-17 12:52:34 -0800

    Update project to the latest .NET SDK.

---
+++ BinarySearchEx.Test/ReadOnlyListExtensions.cs
@@ -0,0 +1,142 @@
+﻿namespace ModernRoute.BinarySearchEx.Test
+{
+    [TestFixture]
+    public class ListExtensionsTest
+    {
+        [TestCase(-1, -1, 6, TestName = "EmptyArray")]
+        [TestCase(0, 0, 4, 4, TestName = "OneItemArrayExists")]
+        [TestCase(-2, -2, 5, 3, TestName = "OneItemArrayDoesNotExist")]
+        [TestCase(3, 3, 1394, -10, 50, 128, 1394, 222442342, TestName = "MultipleItemsMiddle")]
+        [TestCase(0, 0, -42, -42, 0, 23424353, 234234532, 234234235, TestName = "MultipleItemsFirst")]
+        [TestCase(4, 4, 234234635, -42, 0, 23424353, 234234532, 234234635, TestName = "MultipleItemsLast")]
+        [TestCase(1, 4, 23424353, 0, 23424353, 23424353, 23424353, 23424353, TestName = "MultipleItemsDuplicates")]
+        [TestCase(4, 4, int.MaxValue, 0, 23424353, 23424353, 23424353, int.MaxValue, TestName = "MultipleItemsMaxValue")]
+        [TestCase(0, 0, int.MinValue, 0, 23424353, 23424353, 23424353, int.MinValue, TestName = "MultipleItemsMinValue")]
+        [TestCase(-1, -1, -1, 0, 1, 3, 8, 12, TestName = "LowerItemThanOthersInArray")]
+        [TestCase(-6, -6, 13, 0, 1, 3, 8, 12, TestName = "HigherItemThanOthersInArray")]
+        [TestCaseSource(nameof(GetFuzzCases), Category = "FuzzCases")]
+        public void CommonCase(int expectedIndexFirst, int expectedIndexLast,
+            int providedValue, params int[] values)
+        {
+            int[] copyValues = new int[values.Length];
+            Array.Copy(values, copyValues, values.Length);
+            Array.Sort(copyValues);
+            Comparer<int> comparer = Comparer<int>.Default;
+            Array.Sort(values, comparer);
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.That(values.BinarySearchFirst(providedValue, comparer), Is.EqualTo(expectedIndexFirst));
+                Assert.That(values.BinarySearchLast(providedValue, comparer), Is.EqualTo(expectedIndexLast));
+                Assert.That(values.BinarySearchFirst(providedValue), Is.EqualTo(expectedIndexFirst));
+                Assert.That(values.BinarySearchLast(providedValue), Is.EqualTo(expectedIndexLast));
+            }
+        }
+
+        [Test]
+        public void UnsortedArray()
+        {
+            List<int> array = [];
+            for (int i = 0; i < 5; ++i)
+            {
+                array.Add(~i);
+            }
+
+            array.BinarySearchFirst(-3, Comparer<int>.Default);
+            array.BinarySearchLast(-3, Comparer<int>.Default);
+            array.BinarySearchFirst(-3);
+            array.BinarySearchLast(-3);
+        }
+
+        [Test]
+        public void UncomparableType()
+        {
+            List<ListExtensionsTest> list = [new ListExtensionsTest()];
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.Throws<InvalidOperationException>(() =>
+                {
+                    list.BinarySearchFirst(new ListExtensionsTest());
+                });
+                Assert.Throws<InvalidOperationException>(() =>
+                {
+                    list.BinarySearchLast(new ListExtensionsTest());
+                });
+            }
+        }
+
+        [TestCase(-1, 0, 3, TestName = "IndexLessThenZero")]
+        [TestCase(0, -1, 3, 3, TestName = "CountLessThenZero")]
+        [TestCase(0, 2, 3, 1, TestName = "IndexCountDoNotDenote")]
+        public void OutOfRange(int index, int count, int item, params int[] values)
+        {
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.Throws<ArgumentOutOfRangeException>(() =>
+                {
+                    values.BinarySearchFirst(index, count, item);
+                });
+                Assert.Throws<ArgumentOutOfRangeException>(() =>
+                {
+                    values.BinarySearchLast(index, count, item);
+                });
+            }
+        }
+
+        private static IEnumerable<object[]> GetFuzzCases()
+        {
+            {
+                int[] array = GetBigArrayOfEvenIntegersWithoutDuplicates();
+                yield return [-61713, -61713, 23423, array];
+                yield return [27339, 27339, -45322, array];
+            }
+
+            Random random = new();
+
+            for (int i = 1; i < 100; ++i)
+            {
+                int[] array = GetBigArrayOfEvenUnsignedIntegersWithDuplicates(i);
+                int index = random.Next(i) << 1;
+                yield return [~(index + 2), ~(index + 2), array[index] + 1, array];
+                yield return [~(index + 2), ~(index + 2), array[index + 1] + 1, array];
+            }
+
+            for (int i = 1; i < 100; ++i)
+            {
+                int[] array = GetBigArrayOfUnsignedIntegersWithoutDuplicates(i);
+                int index = random.Next(i) << 1;
+                yield return [index, index + 1, array[index], array];
+            }
+        }
+
+        private static int[] GetBigArrayOfEvenIntegersWithoutDuplicates()
+        {
+            int[] array = new int[100000];
+            for (int i = -50000; i < 50000; i++)
+            {
+                array[50000 + i] = i << 1;
+            }
+
+            return array;
+        }
+
+        private static int[] GetBigArrayOfEvenUnsignedIntegersWithDuplicates(int halfLength)
+        {
+            int[] array = new int[2 * halfLength];
+            for (int j = 0; j < array.Length; ++j)
+            {
+                array[j] = j & ~1;
+            }
+            return array;
+        }
+
+        private static int[] GetBigArrayOfUnsignedIntegersWithoutDuplicates(int halfLength)
+        {
+            int[] array = new int[2 * halfLength];
+            for (int j = 0; j < array.Length; ++j)
+            {
+                array[j] = j >> 1;
+            }
+            return array;
+        }
+    }
+}
---
+++ BinarySearchEx/IReadOnlyListExtensions.cs
@@ -0,0 +1,217 @@
+﻿
+namespace ModernRoute.BinarySearchEx
+{
+    /// <summary>
+    /// Extensions for <see cref="IReadOnlyList&lt;T&gt;"/>
+    /// </summary>
+    public static class IReadOnlyListExtensions
+    {
+        /// <summary>
+        /// Searches the entire sorted <see cref="IReadOnlyList&lt;T&gt;"/> for an element
+        /// using the specified comparer and returns the nearest to to beginning zero-based
+        /// index of the element.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the list.</typeparam>
+        /// <param name="array">The sorted <see cref="IReadOnlyList&lt;T&gt;"/> to search</param>
+        /// <param name="item">The object to locate. The value can be null for reference types.</param>
+        /// <param name="comparer">
+        /// <p>The <see cref="IComparer&lt;T&gt;"/> implementation to use when comparing elements.</p>
+        /// <p>-or-</p>
+        /// <p><b>null</b> to use the default comparer <see cref="Comparer&lt;T&gt;.Default"/>.</p>
+        /// </param>
+        /// <returns>
+        /// The nearest to beginning zero-based index of item in the sorted <see cref="IReadOnlyList&lt;T&gt;"/>,
+        /// if <paramref name="item"/> is found; otherwise, a negative number that is the bitwise complement
+        /// of the index of the next element that is larger than <paramref name="item"/> or, if there is no
+        /// larger element, the bitwise complement of <see cref="IReadOnlyList&lt;T&gt;.Count"/>.
+        /// </returns>
+        public static int BinarySearchFirst<T>(
+            this IReadOnlyList<T> array, T item, IComparer<T>? comparer = null)
+        {
+            return array.BinarySearchFirst(0, array.Count, item, comparer);
+        }
+
+        /// <summary>
+        /// Searches the entire sorted <see cref="IReadOnlyList&lt;T&gt;"/> for an element
+        /// using the specified comparer and returns the nearest to ending zero-based
+        /// index of the element.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the list.</typeparam>
+        /// <param name="array">The sorted <see cref="IReadOnlyList&lt;T&gt;"/> to search</param>
+        /// <param name="item">The object to locate. The value can be null for reference types.</param>
+        /// <param name="comparer">
+        /// <p>The <see cref="IComparer&lt;T&gt;"/> implementation to use when comparing elements.</p>
+        /// <p>-or-</p>
+        /// <p><b>null</b> to use the default comparer <see cref="Comparer&lt;T&gt;.Default"/>.</p>
+        /// </param>
+        /// <returns>
+        /// The nearest to ending zero-based index of item in the sorted <see cref="IReadOnlyList&lt;T&gt;"/>,
+        /// if <paramref name="item"/> is found; otherwise, a negative number that is the bitwise complement
+        /// of the index of the next element that is larger than <paramref name="item"/> or, if there is no
+        /// larger element, the bitwise complement of <see cref="IReadOnlyList&lt;T&gt;.Count"/>.
+        /// </returns>
+        public static int BinarySearchLast<T>(
+            this IReadOnlyList<T> array, T item, IComparer<T>? comparer = null)
+        {
+            return array.BinarySearchLast(0, array.Count, item, comparer);
+        }
+
+        /// <summary>
+        /// Searches a range of elements in the sorted <see cref="IReadOnlyList&lt;T&gt;"/> for an
+        /// element using the specified comparer and returns the nearest to beginning
+        /// zero-based index of the element.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the list.</typeparam>
+        /// <param name="array">The sorted <see cref="IReadOnlyList&lt;T&gt;"/> to search</param>
+        /// <param name="index">The zero-based starting index of the range to search.</param>
+        /// <param name="count">The length of the range to search.</param>
+        /// <param name="item">The object to locate. The value can be null for reference types.</param>
+        /// <param name="comparer">
+        /// <p>The <see cref="IComparer&lt;T&gt;"/> implementation to use when comparing elements.</p>
+        /// <p>-or-</p>
+        /// <p><b>null</b> to use the default comparer <see cref="Comparer&lt;T&gt;.Default"/>.</p>
+        /// </param>
+        /// <returns>
+        /// The nearest to beginning zero-based index of item in the sorted <see cref="IReadOnlyList&lt;T&gt;"/>,
+        /// if <paramref name="item"/> is found; otherwise, a negative number that is the bitwise complement
+        /// of the index of the next element that is larger than <paramref name="item"/> or, if there is no
+        /// larger element, the bitwise complement of <see cref="IReadOnlyList&lt;T&gt;.Count"/>.
+        /// </returns>
+        public static int BinarySearchFirst<T>(
+            this IReadOnlyList<T> array, int index, int count, T item, IComparer<T>? comparer = null)
+        {
+            ArgumentOutOfRangeException.ThrowIfLessThan(index, 0);
+            ArgumentOutOfRangeException.ThrowIfLessThan(count, 0);
+            ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(index + count - 1, array.Count);
+            comparer ??= Comparer<T>.Default;
+
+            try
+            {
+                int left = index;
+                int right = index + count - 1;
+
+                if (left > right)
+                {
+                    return ~0;
+                }
+
+                while (right - left > 0)
+                {
+                    int middle = left + (right - left) / 2;
+
+                    if (comparer.Compare(item, array[middle]) > 0)
+                    {
+                        left = middle + 1;
+                    }
+                    else
+                    {
+                        right = middle;
+                    }
+                }
+
+                int res = comparer.Compare(item, array[left]);
+
+                if (res > 0)
+                {
+                    return ~(left + 1);
+                }
+                else if (res < 0)
+                {
+                    return ~left;
+                }
+                else
+                {
+                    return left;
+                }
+            }
+            catch (Exception ex) when (!IsCritical(ex))
+            {
+                throw new InvalidOperationException("Failed to compare two elements.", ex);
+            }
+        }
+
+        /// <summary>
+        /// Searches a range of elements in the sorted <see cref="IReadOnlyList&lt;T&gt;"/> for an
+        /// element using the specified comparer and returns the nearest to ending
+        /// zero-based index of the element.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the list.</typeparam>
+        /// <param name="array">The sorted <see cref="IReadOnlyList&lt;T&gt;"/> to search</param>
+        /// <param name="index">The zero-based starting index of the range to search.</param>
+        /// <param name="count">The length of the range to search.</param>
+        /// <param name="item">The object to locate. The value can be null for reference types.</param>
+        /// <param name="comparer">
+        /// <p>The <see cref="IComparer&lt;T&gt;"/> implementation to use when comparing elements.</p>
+        /// <p>-or-</p>
+        /// <p><b>null</b> to use the default comparer <see cref="Comparer&lt;T&gt;.Default"/>.</p>
+        /// </param>
+        /// <returns>
+        /// The nearest to ending zero-based index of item in the sorted <see cref="IReadOnlyList&lt;T&gt;"/>,
+        /// if <paramref name="item"/> is found; otherwise, a negative number that is the bitwise complement
+        /// of the index of the next element that is larger than <paramref name="item"/> or, if there is no
+        /// larger element, the bitwise complement of <see cref="IReadOnlyList&lt;T&gt;.Count"/>.
+        /// </returns>
+        public static int BinarySearchLast<T>(
+            this IReadOnlyList<T> array, int index, int count, T item, IComparer<T>? comparer = null)
+        {
+            ArgumentOutOfRangeException.ThrowIfLessThan(index, 0);
+            ArgumentOutOfRangeException.ThrowIfLessThan(count, 0);
+            ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(index + count - 1, array.Count);
+            comparer ??= Comparer<T>.Default;
+
+            try
+            {
+                int left = index;
+                int right = index + count - 1;
+
+                if (left > right)
+                {
+                    return ~0;
+                }
+
+                while (right - left > 0)
+                {
+                    int middle = right - (right - left) / 2;
+
+                    if (comparer.Compare(item, array[middle]) < 0)
+                    {
+                        right = middle - 1;
+                    }
+                    else
+                    {
+                        left = middle;
+                    }
+                }
+
+                int res = comparer.Compare(item, array[right]);
+
+                if (res > 0)
+                {
+                    return ~(right + 1);
+                }
+                else if (res < 0)
+                {
+                    return ~right;
+                }
+                else
+                {
+                    return right;
+                }
+            }
+            catch (ArgumentException ex) when (!IsCritical(ex))
+            {
+                throw new InvalidOperationException("Failed to compare two elements.", ex);
+            }
+        }
+
+        private static bool IsCritical(Exception ex)
+        {
+            return ex is OutOfMemoryException ||
+                   ex is AppDomainUnloadedException ||
+                   ex is BadImageFormatException ||
+                   ex is CannotUnloadAppDomainException ||
+                   ex is InvalidProgramException ||
+                   ex is ThreadAbortException;
+        }
+    }
+}
